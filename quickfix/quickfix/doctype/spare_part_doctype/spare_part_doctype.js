c167c233b2a4e0bfa5dcca858e3a0c829ab0ab5d
Siddarth5100 <siddarthsiddhu5100@gmail.com>
2026-06-01 19:26:37 +0530

    feat: create doctype for service invoice with required fields

--- quickfix/quickfix/doctype/spare_part_doctype/spare_part_doctype.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, JC Siddarth and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Spare Part Doctype", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- quickfix/quickfix/doctype/spare_part_doctype/spare_part_doctype.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, JC Siddarth and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class SparePartDoctype(Document):
-	pass
