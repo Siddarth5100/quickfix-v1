344eb330c01f48e90bcc054ee9549109584d57bc
Siddarth5100 <siddarthsiddhu5100@gmail.com>
2026-06-03 19:05:41 +0530

    feat: add logic to filter active technicians with specialization

--- quickfix/service_center/doctype/job_card/job_card.js
+++ quickfix/service_center/doctype/job_card/job_card.js
@@ -6,3 +6,35 @@
 
 // 	},
 // });
+
+// H1 - Job Card Form Script
+// setup handler: assign technician based on the matching specilization
+console.log("-----------Jobcard js loaded")
+
+frappe.ui.form.on("Job Card", {
+    setup(frm) {
+        frm.set_query("assigned_technician", function() {
+            return {
+                filters: {
+                    status: "Active",
+                    specialization: frm.doc.device_type
+                }
+            }
+        });
+    },
+
+    assigned_technician: function(frm) {
+        let tech = frm.doc.assigned_technician;
+        let device = frm.doc.device_type;
+
+        frappe.db.get_value("Technician", tech, "specialization")
+
+        .then(r => {
+            let specialization = r.message.specialization;
+
+            if (specialization != device) {
+                frappe.msgprint("There is no technician in this specialization")
+            }
+        });
+    }
+});
