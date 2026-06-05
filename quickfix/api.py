28ec82e01d5e7016ad7ed6a28413419440f42074
Siddarth5100 <siddarthsiddhu5100@gmail.com>
2026-06-05 07:22:37 +0530

    feat: implement overdue jobs query using frappe.qb

--- quickfix/api.py
+++ quickfix/api.py
@@ -1,4 +1,26 @@
 import frappe
+from frappe.utils import nowdate, add_days
+from frappe.query_builder import DocType
+
+# Part B - frappe.qb Query Builder
+# Write a function get_overdue_jobs() using frappe.qb
+def get_overdue_jobs():
+    JC = DocType("Job Card")
+
+    cutoff_date = add_days(nowdate(), -7)
+
+    overdue_jobs = (
+        frappe.qb.from_(JC)
+        .select(JC.name, JC.customer_name, JC.assigned_technician, JC.creation)
+        .where(
+            JC.status.isin(["Pending Diagnosis", "In Repair"])
+            & (JC.creation < cutoff_date)
+        )
+        .orderby(JC.creation.asc())
+    ).run(as_dict= True)
+
+    return overdue_jobs    
+
 
 # F4 - override_whitelisted_methods Hook
 @frappe.whitelist()
