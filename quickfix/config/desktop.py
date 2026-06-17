9cf9fd2e06a87895c274cf478d30fc8e5702113a
Siddarth5100 <siddarthsiddhu5100@gmail.com>
2026-06-17 11:45:37 +0530

    feat: add .js files for asset hooks and desktop.py in config

---
+++ quickfix/config/desktop.py
@@ -0,0 +1,8 @@
+def get_data():
+    return [
+        {
+            "module_name": "Service Center",
+            "label": "Service Center",
+            "type": "module"
+        }
+    ]
---
+++ quickfix/public/js/job_card.js
@@ -0,0 +1,6 @@
+// test page
+frappe.ui.form.on("Job Card", {
+    refresh(frm) {
+        console.log("Job card form Loaded")
+    }
+})
