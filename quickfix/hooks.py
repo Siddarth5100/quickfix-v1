028c6bc896140a83fa92feba034e77ca68e1fc15
Siddarth5100 <siddarthsiddhu5100@gmail.com>
2026-06-17 11:18:34 +0530

    feat: add path in hooks file for asset hooks

--- quickfix/hooks.py
+++ quickfix/hooks.py
@@ -71,6 +71,8 @@ app_include_js = "/assets/quickfix/js/quickfix.js"
 # web_include_css = "/assets/quickfix/css/quickfix.css"
 # web_include_js = "/assets/quickfix/js/quickfix.js"
 
+web_include_js = "/assets/quickfix/js/web.js"
+
 # include custom scss in every website theme (without file extension ".scss")
 # website_theme_scss = "quickfix/public/scss/website"
 
