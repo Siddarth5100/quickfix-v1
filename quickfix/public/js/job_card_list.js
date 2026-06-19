a1c9ead6e3e0c0105094613bcddc3ce331f3a550
Siddarth5100 <siddarthsiddhu5100@gmail.com>
2026-06-19 06:55:51 +0530

    feat: add color indicator for status in job card

--- quickfix/public/js/job_card_list.js
+++ quickfix/public/js/job_card_list.js
@@ -1,5 +1,20 @@
+console.log("doctype_list_js ------------ loaded")
+
+// want to check how to change color in UI
 frappe.listview_settings["Job Card"] = {
-    onload(listview) {
-        console.log("Job Card List View Loaded")
+    get_indicator(doc) {
+        console.log(doc.status)
+
+        if (doc.status == "Ready for Delivery") {
+            return ["Ready for Delivery", "blue"];
+        } 
+
+        if (doc.status == "Delivered") {
+            return ["Delivered", "green"]
+        }
+
+        if (doc.status == "Pending Diagnosis") {
+            return["Pending Diagnosis", "green"]
+        }
     }
 }
