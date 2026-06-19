ab2954871d2ce5a9d876658a6a0981fcdae2623d
Siddarth5100 <siddarthsiddhu5100@gmail.com>
2026-06-19 06:58:10 +0530

    chore: remove debug prints

--- quickfix/public/js/job_card_list.js
+++ quickfix/public/js/job_card_list.js
@@ -1,9 +1,6 @@
-console.log("doctype_list_js ------------ loaded")
-
 // want to check how to change color in UI
 frappe.listview_settings["Job Card"] = {
     get_indicator(doc) {
-        console.log(doc.status)
 
         if (doc.status == "Ready for Delivery") {
             return ["Ready for Delivery", "blue"];
